e01f50fce46f9e43e0ffdbfe1275a765dfe0ed6d
Harshbir Singh <singh.hveer1@gmail.com>
2022-04-17 16:46:16 -0600

    cohort with the longest average duration of assistance requests

---
+++ 4_queries/cohort_longest_assistance.sql
@@ -0,0 +1,9 @@
+-- cohort with the longest average duration of assistance requests
+
+SELECT cohorts.name as name , AVG(completed_at - started_at) AS average_assistance_time
+FROM assistance_requests 
+JOIN students ON student_id = students.id 
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
