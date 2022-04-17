34f3a895ca2930613f60ffa4dcf0159f36e8a6f4
Harshbir Singh <singh.hveer1@gmail.com>
2022-04-17 16:41:56 -0600

    Total number of assistance_requests for a teacher

---
+++ 4_queries/total_teacher_assistance.sql
@@ -0,0 +1,7 @@
+-- total number of assistance_requests for a teacher
+
+SELECT COUNT(assistance_requests.id) as total_assistances, teachers.name as name 
+FROM assistance_requests JOIN teachers
+ON teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY name;
