6f8db80ad53c394afc20e9c6927e1e3fc93332bf
Harshbir Singh <singh.hveer1@gmail.com>
2022-04-19 02:05:13 -0600

    Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,9 +1,18 @@
 const { argv } = require('process');
 const { Pool } = require('pg');
 
+//Parameterized Query
+
 let args = process.argv.slice(2);
-let cohort = args[0];
-let limit = args[1];10
+let values = [`${args[0]}%`, args[1]]
+
+let text = `SELECT students.id, students.name, cohorts.name as cohort_id
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+group by students.id, students.name, cohorts.name
+LIMIT $2;`
+
 
 const pool = new Pool({
   user: 'veersingh',
@@ -11,14 +20,7 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort_id
-FROM students
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohort}%'
-group by students.id, students.name, cohorts.name
-LIMIT ${limit};
-`)
+pool.query(text, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,22 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-pool.query(`
+
+// Parameterized Query
+const values = [`${process.argv[2] || 'JUL02'}`];
+
+const text = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests.*) as total_assistances
 FROM teachers 
 JOIN assistance_requests on assistance_requests.teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 GROUP BY teacher, cohort
 ORDER BY teacher;
-`)
+`;
+
+pool.query(text, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} : ${user.teacher}`);
